f4993c969546e4c4e542243f5d7b0f0751f65acc
Davis <[agdaemon@gmail.com]>
2024-02-24 00:57:40 +0900

    favicon change

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,23 +1,27 @@
 @extends('layouts.app')
-
+@section('title','HOME')
 @section('content')
 <div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
+	<div class="row justify-content-center">
+		<div class="col-md-8">
+			<div class="card bg-dark text-white">
+				<div class="card-header">{{ __('Dashboard') }}</div>
 
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
+				<div class="card-body">
+					@if (session('status'))
+						<div class="alert alert-success" role="alert">
+							{{ session('status') }}
+						</div>
+					@endif
 
-                    {{ __('You are logged in!') }}
-                </div>
-            </div>
-        </div>
-    </div>
+					ログインしました。
+					<form action="{{ route('logout') }}" method="POST">
+					@csrf
+					<button type="submit" class="btn btn-primary">ログアウト</button>
+					</form>
+				</div>
+			</div>
+		</div>
+	</div>
 </div>
 @endsection
